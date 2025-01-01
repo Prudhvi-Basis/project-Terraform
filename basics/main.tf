39046b06847cc4eee2831637ea818cacfa937e84
prudhi4321 <prudhviraj3525@gmail.com>
2025-01-01 18:03:24 +0530

    m

--- basics/main.tf
+++ basics/main.tf
@@ -6,6 +6,13 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 }
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z00196431INWTJ0O5YT57"
+  name    = "frontend-dev.devops11.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
 resource "aws_instance" "mongodb" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -13,6 +20,13 @@ resource "aws_instance" "mongodb" {
   tags = {
     Name = "mongodb"
   }
+}
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z00196431INWTJ0O5YT57"
+  name    = "mongodb-dev.devops11.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
 }
 resource "aws_instance" "catalogue" {
   ami = "ami-09c813fb71547fc4f"
@@ -22,6 +36,13 @@ resource "aws_instance" "catalogue" {
     Name = "catalogue"
   }
 }
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z00196431INWTJ0O5YT57"
+  name    = "catalogue-dev.devops11.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
 resource "aws_instance" "user" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -29,6 +50,13 @@ resource "aws_instance" "user" {
   tags = {
     Name = "user"
   }
+}
+resource "aws_route53_record" "user" {
+  zone_id = "Z00196431INWTJ0O5YT57"
+  name    = "user-dev.devops11.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
 }
 resource "aws_instance" "cart" {
   ami = "ami-09c813fb71547fc4f"
@@ -38,6 +66,13 @@ resource "aws_instance" "cart" {
     Name = "cart"
   }
 }
+resource "aws_route53_record" "cart" {
+  zone_id = "Z00196431INWTJ0O5YT57"
+  name    = "cart-dev.devops11.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
+}
 resource "aws_instance" "redis" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -45,6 +80,13 @@ resource "aws_instance" "redis" {
   tags = {
     Name = "redis"
   }
+}
+resource "aws_route53_record" "redis" {
+  zone_id = "Z00196431INWTJ0O5YT57"
+  name    = "redis-dev.devops11.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
 }
 resource "aws_instance" "rabbitmq" {
   ami = "ami-09c813fb71547fc4f"
@@ -54,6 +96,13 @@ resource "aws_instance" "rabbitmq" {
     Name = "rabbitmq"
   }
 }
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z00196431INWTJ0O5YT57"
+  name    = "rabbitmq-dev.devops11.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
 resource "aws_instance" "payment" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -61,6 +110,13 @@ resource "aws_instance" "payment" {
   tags = {
     Name = "payment"
   }
+}
+resource "aws_route53_record" "payment" {
+  zone_id = "Z00196431INWTJ0O5YT57"
+  name    = "payment-dev.devops11.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
 }
 resource "aws_instance" "shipping" {
   ami = "ami-09c813fb71547fc4f"
@@ -70,6 +126,13 @@ resource "aws_instance" "shipping" {
     Name = "shipping"
   }
 }
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z00196431INWTJ0O5YT57"
+  name    = "shipping-dev.devops11.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
 resource "aws_instance" "dispatch" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -78,6 +141,13 @@ resource "aws_instance" "dispatch" {
     Name = "dispatch"
   }
 }
+resource "aws_route53_record" "dispatch" {
+  zone_id = "Z00196431INWTJ0O5YT57"
+  name    = "dispatch-dev.devops11.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.dispatch.private_ip]
+}
 resource "aws_instance" "mysql" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -85,4 +155,11 @@ resource "aws_instance" "mysql" {
   tags = {
     Name = "mysql"
   }
+}
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z00196431INWTJ0O5YT57"
+  name    = "mysql-dev.devops11.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
 }
