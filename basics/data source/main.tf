43a5aefdb1e957279528c013f77ff4630ced2c1b
prudhi4321 <prudhviraj3525@gmail.com>
2025-01-02 13:24:27 +0530

    Terraform

--- basics/data source/main.tf
+++ basics/data source/main.tf
@@ -8,6 +8,8 @@ resource "aws_instance" "server" {
 }
 data "aws_ami" "ami" {
 
-  most_recent      = true
-  name_regex       = "RHEL-9-DevOps-Practice"
-  owners           = ["973714476881"]
+  most_recent = true
+  name_regex  = "RHEL-9-DevOps-Practice"
+  owners = ["973714476881"]
+
+}
