90b3b7f711f63cba13af5325786ba89567d75e7e
prudhi4321 <prudhviraj3525@gmail.com>
2025-01-02 18:59:00 +0530

    Terraform

--- project-v1/roboshop-module/moduleA/main.tf
+++ project-v1/roboshop-module/moduleA/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "instance" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-0b308c7134616a7ce"]
