1e25177f75ad10c70cedbdbd8bdcb3cfb07e0904
prudhi4321 <prudhviraj3525@gmail.com>
2025-01-02 20:24:13 +0530

    Terraform

--- project-v1/roboshop-module/moduleB/main.tf
+++ project-v1/roboshop-module/moduleB/main.tf
@@ -11,7 +11,12 @@ module "ec2" {
   source = "./ec2"
   instance_name = var.instances[count.index]
 }
-#
+output "ip" {
+  value = module.ec2
+}
 # module "route53" {
+#   count = length(var.instances)
 #   source = "./route53"
+#   instance_name = var.instances[count.index]
+#   ip_address = module.ec2[count.index].ip_address
 # }
