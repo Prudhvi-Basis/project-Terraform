23834dc602ec79e551c33aa71fd9a71092790516
prudhi4321 <prudhviraj3525@gmail.com>
2025-01-03 16:22:30 +0530

    Terrform

--- Roboshop-terraform/multi-env-code-structure/env-dev/state.tfvars
+++ Roboshop-terraform/multi-env-code-structure/env-dev/state.tfvars
@@ -1,3 +1,3 @@
 bucket = "terraform11"
-key    = "sample/dev/terraform.tfstate"
+key    = "roboshop/dev/terraform.tfstate"
 region = "us-east-1"
--- Roboshop-terraform/multi-env-code-structure/modules/ec2/main.tf
+++ Roboshop-terraform/multi-env-code-structure/modules/ec2/main.tf
@@ -1,11 +1,37 @@
+resource "aws_security_group" "sg" {
+  name        = "${var.component_name}-${var.env}-sg"
+  description = "Inbound allow for ${var.component_name}"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+  ingress {
+    from_port        = var.app_port
+    to_port          = var.app_port
+    protocol         = "TCP"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+}
+
 resource "aws_instance" "instance" {
-  ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t3.small"
+  ami           = data.aws_ami.ami.id
+  instance_type = var.instance_type
   vpc_security_group_ids = ["sg-0b308c7134616a7ce"]
   tags = {
-    Name = "test-${var.env}"
+    Name = "${var.component_name}-${var.env}"
 
   }
 
 }
-variable "env" {}
