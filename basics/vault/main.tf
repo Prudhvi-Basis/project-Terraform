311174f569f55a6ff8c3915b6f219b1109da5ad3
prudhi4321 <prudhviraj3525@gmail.com>
2025-01-04 16:56:42 +0530

    Vault

---
+++ basics/vault/main.tf
@@ -0,0 +1,12 @@
+
+provider "vault" {
+  address = "https://172.31.91.125:8200"
+  token = var.vault_token
+  skip_tls_verify = true
+}
+variable "vault_token" {}
+
+data "vault_kv_secret_v2" "example" {
+  mount = "my_credentials"
+  name  = "test"
+}
